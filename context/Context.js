54a5e3a2530917176ee4acafb09697511facf3c0
Filali Med Amar <medamarfilali@gmail.com>
2022-01-28 13:46:57 +0100

    Added transports

--- context/Context.js
+++ context/Context.js
@@ -5,10 +5,12 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io(process.env.NEXT_PUBLIC_HOST_SERVER);
-io.connect("/", {
+const socket = io(process.env.NEXT_PUBLIC_HOST_SERVER, {
+  // Send auth token on connection, you will need to DI the Auth service above
+  // 'query': 'token=' + Auth.getToken()
+  path: "/socket.io",
+  transports: ["websocket"],
   secure: true,
-  transports: ["flashsocket", "polling", "websocket"],
 });
 
 const ContextProvider = ({ children }) => {
