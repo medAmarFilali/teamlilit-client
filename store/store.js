03a558a16596bbda0885e9f9ebdb961598f0ddd9
Filali Med Amar <75205762+medAmarFilali@users.noreply.github.com>
2025-06-04 11:40:34 +0100

    ignore lock files

--- store/store.js
+++ store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
